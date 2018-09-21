12165a6def555195804dc35d696cb2c0ad67b832
Kyle Meserve <KyleMeserve@Kyles-MacBook-Air.local>
2018-09-21 17:41:11 -0400

    Unique classes are built and tested

--- fists_of_fire/js/app.js
+++ fists_of_fire/js/app.js
@@ -21,21 +21,45 @@ class Fighter {
     }
 };
 
-// Build Character Class extending on Fighter for every Unique Character (Add the ability)
+// Build Character Class extending on Fighter for every Unique Character (Add the ability) (-- Tested with Logs already! All functions and stats are retrievable --)
 class Brawler extends Fighter {
+    //Special Attack
     special(){
         console.log('The special attack has been used by the Brawler');
     }
 }
-const testBrawler = new Brawler('Billy');
+class Priest extends Fighter {
+    //Special Attack
+    special(){
+        console.log('The special attack has been used by the Priest');
+    }
+}
+class Knight extends Fighter {
+    //Special Attack
+    special(){
+        console.log('The special attack has been used by the Knight');
+    }
+}
+class Ranger extends Fighter {
+    //Special Attack
+    special(){
+        console.log('The special attack has been used by the Ranger');
+    }
+}
+
+//List of Playable Characters, one for each player.
+const testBrawler = new Brawler('GARR');
+const testPriest = new Priest('Lillian');
+const testKnight = new Knight('Sir Grant');
+const testRanger = new Ranger('Outlaw');
 console.log(testBrawler);
-console.log(testBrawler.name);
-console.log(testBrawler.health);
-console.log(testBrawler.attack);
-testBrawler.fight();
-testBrawler.counter();
-testBrawler.heal();
+console.log(testPriest);
+console.log(testKnight);
+console.log(testRanger);
 testBrawler.special();
+testPriest.special();
+testKnight.special();
+testRanger.special();
 // Build a Fight Function that takes User Inputs and converts them to actions from the Fighter class.
 
 // Build a function that runs the outcome of the inputs and alters the character's stats (health).
