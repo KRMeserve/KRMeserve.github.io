bca904441a159e529e688be6f6a4e408b4b0344c
Kyle Meserve <KyleMeserve@Kyles-MacBook-Air.local>
2018-12-21 14:37:49 -0500

    have picture working and links to facebook and pinterest

--- cathrine/app.js
+++ cathrine/app.js
@@ -1,6 +1,10 @@
 $(()=>{
-    console.log('connected to jQuery');
+    const arrayOfPics = ['1', '2', '3', '4', '5', '6','7','8','9','10','11','12','13','14','15','16','17','18','19','20','21','22','23','24'];
+
+    const randomNumber = Math.floor(Math.random() * arrayOfPics.length) + 1;
+
+    $('.random-picture').append('<img>').attr('src', 'img/' + randomNumber + '.jpg');
+
+    console.log('connected');
+
 });
-
-
-console.log('connected');
