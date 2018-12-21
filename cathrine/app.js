3c9afc7098a7785ae6903cada1b4364e7396c003
Kyle Meserve <KyleMeserve@Kyles-MacBook-Air.local>
2018-12-21 16:09:57 -0500

    have links working, css worked on

--- cathrine/app.js
+++ cathrine/app.js
@@ -3,7 +3,40 @@ $(()=>{
 
     const randomNumber = Math.floor(Math.random() * arrayOfPics.length) + 1;
 
+    let quote = '';
+    let author = '';
+
+    const quoteOfTheDay = ()=>{
+        fetch('https://talaikis.com/api/quotes/random/')
+        // fetch('https://healthruwords.p.mashape.com/v1/quotes/')
+            .then(function(response) {
+                console.log(response);
+                return response.json();
+        })
+            .then(function(myJson) {
+                console.log(myJson);
+                console.log(JSON.stringify(myJson));
+                $('.quote').text(myJson.quote);
+                $('.author').text(myJson.author);
+                return JSON.stringify(myJson);
+        });
+    }
+
+    quoteOfTheDay();
+
     $('.random-picture').append('<img>').attr('src', 'img/' + randomNumber + '.jpg');
+
+    const date = new Date();
+    const hourNow = date.getHours()
+
+    if (hourNow > 17) {
+        $('.greeting').text('Good Evening, Cathrine!')
+    } else if (hourNow > 11) {
+        $('.greeting').text('Good Afternoon, Cathrine!')
+    } else {
+        $('.greeting').text('Good Morning, Cathrine!')
+    }
+
 
     console.log('connected');
 
